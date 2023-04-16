94e2afbc0f03c047f7ce3ad1036bca02cf667883
diepyon <tomarimashita@gmail.com>
2023-04-16 16:37:51 +0900

    installed vue

---
+++ src/resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Hello from Vue.js!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
---
+++ src/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Hello from Vue.js 3!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ExampleComponent'
+  }
+  </script>
+  
